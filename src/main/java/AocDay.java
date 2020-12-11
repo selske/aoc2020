5bc0f76e2f93f2084932272ee84d4a67ee452b2f
Kevin Sels <kevin.sels@vrt.be>
2020-12-11 10:02:23 +0100

    downtuned timer

--- src/main/java/AocDay.java
+++ src/main/java/AocDay.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public abstract class AocDay<INPUT> {
 
     abstract INPUT prepareInput() throws Exception;
@@ -20,66 +17,28 @@ public abstract class AocDay<INPUT> {
                 part1(input);
                 part2(input);
             }
-            Timer timer = new Timer();
-            System.out.println("SOLUTIONS:");
-            timer.segment("Prepare");
+            long beforePrepare = System.nanoTime();
             INPUT input = prepareInput();
-            timer.segment("Part 1");
-            System.out.println("Part 1: " + part1(input));
-            timer.segment("Part 2");
-            System.out.println("Part 2: " + part2(input));
-            timer.stop();
+            long afterPrepare = System.nanoTime();
+            long beforePart1 = System.nanoTime();
+            String part1Solution = part1(input);
+            long afterPart1 = System.nanoTime();
+            long beforePart2 = System.nanoTime();
+            String part2Solution = part2(input);
+            long afterPart2 = System.nanoTime();
 
+            System.out.println("SOLUTIONS:");
+            System.out.println("Part1: " + part1Solution);
+            System.out.println("Part2: " + part2Solution);
             System.out.println();
             System.out.println("STATS:");
-            timer.segments.forEach(segment -> System.out.println(segment.name + ": " + (segment.duration() / 1_000_000.) + "ms"));
-            System.out.println("Total time: " + (timer.totalTime() / 1_000_000.) + "ms");
+            System.out.println("Prepare: " + ((afterPrepare - beforePrepare) / 1_000_000.) + "ms");
+            System.out.println("Part1: " + ((afterPart1 - beforePart1) / 1_000_000.) + "ms");
+            System.out.println("Part2: " + ((afterPart2 - beforePart2) / 1_000_000.) + "ms");
+            System.out.println("Total time: " + ((afterPart2 - beforePrepare) / 1_000_000.) + "ms");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    private static class Timer {
-
-        long startTime;
-        long endTime;
-        private final List<Segment> segments = new ArrayList<>();
-        private OngoingSegment currentSegment;
-
-        void segment(String segment) {
-            long currentTime = System.nanoTime();
-            if (startTime == 0) {
-                startTime = currentTime;
-            } else {
-                segments.add(currentSegment.complete(currentTime));
-            }
-            currentSegment = new OngoingSegment(segment, currentTime);
-        }
-
-        void stop() {
-            long currentTime = System.nanoTime();
-            this.endTime = currentTime;
-            segments.add(currentSegment.complete(currentTime));
-        }
-
-        public List<Segment> getSegments() {
-            return segments;
-        }
-
-        public long totalTime() {
-            return endTime - startTime;
-        }
-
-    }
-
-    private static record OngoingSegment(String name, long startTime) {
-
-        public Segment complete(long currentTime) {
-            return new Segment(name, currentTime - startTime);
-        }
-
-    }
-
-    private static record Segment(String name, long duration) {}
 
 }
